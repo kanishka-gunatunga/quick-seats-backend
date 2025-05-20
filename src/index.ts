6029b96d9bee2798be1feba3631f0726501bff93
madushan10 <mnmnjith@gmail.com>
2025-05-20 08:56:14 +0530

    session update on vercel

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,40 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import session from 'express-session';
+import pgSession from 'connect-pg-simple';
+import { Pool } from 'pg';
 import apiRoutes from './routes/api/api.routes';
 import adminRoutes from './routes/admin/admin.routes';
 import path from 'path';
+const PgSession = pgSession(session);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 dotenv.config();
 const app = express();
-
+app.set('trust proxy', 1);
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
+  store: new PgSession({
+    pool: pool,                
+    tableName: 'user_sessions' 
+  }),
   secret: 'quickseats20250516',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax'
+  }
 }));
 
 app.use((req, res, next) => {
