660df5365d5e751a852592f997accba82c4ae719
madushan10 <mnmnjith@gmail.com>
2025-05-22 14:48:55 +0530

    checkout api 5

--- src/controllers/api/checkoutController.ts
+++ src/controllers/api/checkoutController.ts
@@ -6,18 +6,17 @@ import { z } from 'zod';
 const prisma = new PrismaClient();
 
 export const checkout = async (req: Request, res: Response) => {
-    const schema = z
-    .object({
-      first_name: z.string().min(1, 'First name is required'),
-      last_name: z.string().min(1, 'Last name is required'),
-      contact_number: z.string().min(1, 'Contact number is required'),
-      email: z.string({ required_error: 'Email is required' }).email('Invalid email format'),
-      nic_passport: z.string().min(1, 'NIC/Passport is required'),
-      country: z.string().min(1, 'Country is required'),
-      event_id: z.string().min(1, 'Event id is required'),
-      user_id: z.string().min(1, 'User id is required'),
-      seat_ids: z.string().min(1, 'At least one seat must be selected')
-    });
+    const schema = z.object({
+  first_name: z.string().min(1, 'First name is required'),
+  last_name: z.string().min(1, 'Last name is required'),
+  contact_number: z.string().min(1, 'Contact number is required'),
+  email: z.string({ required_error: 'Email is required' }).email('Invalid email format'),
+  nic_passport: z.string().min(1, 'NIC/Passport is required'),
+  country: z.string().min(1, 'Country is required'),
+  event_id: z.string().min(1, 'Event id is required'),
+  user_id: z.string().min(1, 'User id is required'),
+  seat_ids: z.array(z.number()).min(1, 'At least one seat must be selected')
+});
 
   const result = schema.safeParse(req.body);
 
