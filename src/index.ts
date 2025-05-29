71293a1333aff33bee9a6f46fe47be57366b8112
madushan10 <mnmnjith@gmail.com>
2025-05-29 10:47:38 +0530

    ticket assign 13

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,8 @@ const app = express();
 app.set('trust proxy', 1);
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(session({
   store: new PgSession({
