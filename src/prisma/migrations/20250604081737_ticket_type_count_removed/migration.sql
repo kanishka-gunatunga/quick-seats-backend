7d5c5c4004ff03a1421a577f0affa21b2cb124c4
madushan10 <mnmnjith@gmail.com>
2025-06-04 13:52:10 +0530

    admin ticket type update

---
+++ src/prisma/migrations/20250604081737_ticket_type_count_removed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `has_ticket_count` on the `TicketType` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "TicketType" DROP COLUMN "has_ticket_count";
