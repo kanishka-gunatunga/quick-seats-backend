2b8663346addd95f610e245e6a222417e56b593b
madushan10 <mnmnjith@gmail.com>
2025-08-20 14:02:43 +0530

    seatreservation update 1

---
+++ src/prisma/migrations/20250820083139_seat_reservation2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `order_id` on the `SeatReservation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "SeatReservation" DROP COLUMN "order_id";
