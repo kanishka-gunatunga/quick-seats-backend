74e96af956f48bb3cdd27611d0f9790ccef0bf74
madushan10 <mnmnjith@gmail.com>
2026-07-05 18:01:59 +0530

    site enabled

--- src/index_disabled.ts
+++ src/index_disabled.ts
@@ -8,6 +8,7 @@ import apiRoutes from './routes/api/api.routes';
 import adminRoutes from './routes/admin/admin.routes';
 import staffRoutes from './routes/staff/staff.routes';
 import cronRoutes from './routes/cron/cron.routes';
+import fs from 'fs';
 import path from 'path';
 const PgSession = pgSession(session);
 const pool = new Pool({
@@ -51,10 +52,23 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/api', apiRoutes);
-app.use('/', adminRoutes);
-app.use('/staff', staffRoutes);
-app.use('/cron', cronRoutes);
+// app.use('/api', apiRoutes);
+// app.use('/', adminRoutes);
+// app.use('/staff', staffRoutes);
+// app.use('/cron', cronRoutes);
+
+app.use((req, res) => {
+  const filePath = path.join(__dirname, 'public', 'site_unavailable.html');
+
+  // Read the HTML file and send it
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Something went wrong.');
+    } else {
+      res.status(200).setHeader('Content-Type', 'text/html').send(data);
+    }
+  });
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
